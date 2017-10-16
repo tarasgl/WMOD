3fef2c0441b0abd53d5c26f640b4ec4753f27267
tarasgl <31354327+tarasgl@users.noreply.github.com>
2017-10-16 14:27:22 +0300

    moved our project to Django

--- mapapp/static/js/javascript.js
+++ mapapp/static/js/javascript.js
@@ -1,9 +1,5 @@
 function initMap() {
         var LvivCenter = {lat: 49.838, lng: 24.023};
         var map = new google.maps.Map(document.getElementById('map'), { zoom: 9,center: LvivCenter  });
-        
-        
       }
-      $(document).ready(function(){
-      $('.parallax').parallax();
-    });
+    
