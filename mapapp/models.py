d20607054286526f4dd854ed8556c07f3632a1be
tarasgl <31354327+tarasgl@users.noreply.github.com>
2017-10-16 15:17:34 +0300

    added admin page and base for model Marker

--- mapapp/models.py
+++ mapapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Marker(models.Model):
+    title = models.CharField(max_length=80)
+    short_description = models.CharField(max_length=200)
+    full_description = models.TextField()
+    
