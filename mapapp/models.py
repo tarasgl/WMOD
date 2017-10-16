27d2f595080a48b6339b4e660ac1d9f155b1a641
tarasgl <31354327+tarasgl@users.noreply.github.com>
2017-10-16 16:05:55 +0300

    added map into adminpage

--- mapapp/models.py
+++ mapapp/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from location_field.models.plain import PlainLocationField
+
 
 # Create your models here.
 class Marker(models.Model):
     title = models.CharField(max_length=80)
     short_description = models.CharField(max_length=200)
     full_description = models.TextField()
-    
+    city = models.CharField(max_length=255)
+    location = PlainLocationField(based_fields=['city'], zoom=7)
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mapapp',
+    'location_field.apps.DefaultConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
