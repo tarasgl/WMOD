8f1fe1727b6ded79a51f4bcc0b34cfccc067ff13
martysuk <martysuk99@gmail.com>
2017-11-22 10:10:29 +0200

    admin can add pictures in carousel

--- mapapp/views.py
+++ mapapp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
 from .models import Marker
+from .models import Carousel
+
 
 # Create your views here.
 def home_page(request):
     ctx ={
-        'markers': Marker.objects.all()
+        'markers': Marker.objects.all(),
+        'pictures': Carousel.objects.all()
     }
     return render(request, 'mapapp/home_page.html', ctx)
 
